297e036112ef4937520c4a9a6e8c56982e167055
Jonas Geschonke <jonges@uni-bremen.de>
2024-04-16 01:42:49 +0200

    removed astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
 import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: deno(),
-  integrations: [svelte(), tailwind(), partytown(), mdx(), icon(), sitemap()]
+  integrations: [svelte(), tailwind(), partytown(), mdx(), sitemap()]
 });
