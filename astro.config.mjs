e01c5f7e4bbe78311c89f4c149ef561337048b9e
Jonas Geschonke <jonges@uni-bremen.de>
2024-04-16 02:04:10 +0200

    Oooookay readding partytown and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig, passthroughImageService} from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import deno from '@astrojs/deno';
 import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +14,5 @@ export default defineConfig({
   image: {
     service: passthroughImageService()
   },
-  integrations: [svelte(), tailwind(), mdx(), sitemap()]
+  integrations: [svelte(), tailwind(), mdx(), sitemap(), icon(), partytown()]
 });
