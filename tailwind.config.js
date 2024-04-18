e5e29e3a0c7214edc1d65eb833818626a94efa60
Jonas Geschonke <jonges@uni-bremen.de>
2024-04-18 18:17:02 +0200

    base styling and starting layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#f2ece8',
+        'background': '#0e0906',
+        'primary': '#d5ac90',
+        'secondary': '#ff6600',
+        'accent': '#ec9155',
+      },
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 };
